79f7984ce949d2cd8f5bc585cd337501ae49a968
Tal Zer-Aviv <talzeraviv3@gmail.com>
2022-11-23 04:53:55 +0200

    ReactJS connected to ASP.NET + SemanticUI

--- API/Startup.cs
+++ API/Startup.cs
@@ -35,6 +35,12 @@ namespace API
             services.AddDbContext<DataContext>(opt => {
                 opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
             });
+
+            services.AddCors(opt => {
+                opt.AddPolicy("CorsPolicy", policy => {
+                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,6 +54,8 @@ namespace API
             }
 
             app.UseRouting();
+
+            app.UseCors("CorsPolicy");
 
             app.UseAuthorization();
 
--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,24 +1,28 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { Header, List } from 'semantic-ui-react';
 
 function App() {
+  const [aliFinds, setAliFinds] = useState([]);
+
+  useEffect(() => {
+    axios.get('http://localhost:5000/api/alifinds').then(response => {
+      console.log(response);
+      setAliFinds(response.data);
+    })
+  }, [])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p style={{color: 'red'}}>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header as='h2' icon={'users'} content='AliFinds'/>
+        <List>
+          {aliFinds.map((alifind: any) => (
+            <List.Item key={alifind.id}>{alifind.title}</List.Item>
+          ))}
+        </List>
     </div>
   );
 }
